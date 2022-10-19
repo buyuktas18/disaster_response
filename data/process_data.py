07440dc285f31cc8bfeba4c27cfcc61eb58b50b5
buyuktas18 <dbuyuktas.gsl@gmail.com>
2022-10-19 12:56:25 +0000

    initial commit

---
+++ data/process_data.py
@@ -0,0 +1,78 @@
+import sys
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+import numpy as np
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    
+    messages = messages.drop_duplicates(subset="id")
+    categories = categories.drop_duplicates(subset="id")
+    df = pd.merge(messages, categories, how="outer", on="id")
+    
+    temp_df = categories["categories"].str.split(";", expand=True)
+    values = categories.iloc[0].str.split(";")[1]
+    name_of_columns = []
+    for v in values:
+        name_of_columns.append(re.findall('([^-]+)', v)[0])
+
+    temp_df.columns = name_of_columns
+
+    for c in temp_df.columns:
+        temp_df[c] = temp_df[c].apply(lambda x: re.findall('([^-]+)', x)[1])
+        
+    categories = temp_df
+    
+    for column in categories:
+
+        categories[column] = categories[column].astype(int)
+    df.drop(columns="categories", inplace=True)
+ 
+    df.reset_index(inplace=True)
+    
+    categories.reset_index(inplace=True)
+    df.drop(columns="index")
+    df = pd.concat([df, categories], axis=1)
+    df = df[df["related"] != 2]
+    
+    return df
+def clean_data(df):
+    df.drop_duplicates(inplace=True)
+    return df
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' + database_filename + '.db')
+    df.to_sql('disasters', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,111 @@
+import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+# import libraries
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sqlalchemy import create_engine
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import re
+
+def load_data(database_filepath):
+    
+    engine = create_engine('sqlite:///' + database_filepath + '.db')
+    df = pd.read_sql(f"SELECT * FROM disasters", engine)
+    
+    
+    X = df["message"]
+    Y = df.iloc[:,5:]
+    
+   
+    
+
+    return X, Y, list(Y.columns.values)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    y_pred = model.predict(X_test)
+    for i, c in enumerate(category_names):
+        acc = accuracy_score(y_pred[:,i], Y_test[c].values.tolist())
+        f1 = f1_score(y_pred[:,i], Y_test[c].values.tolist())
+        p = precision_score(y_pred[:,i], Y_test[c].values.tolist())
+        r = recall_score(y_pred[:,i], Y_test[c].values.tolist())
+        
+        print(f"for column {c}: ")
+        print('\t' + f"Accuracy: {acc}")
+        print('\t' + f"F1 score: {f1}") 
+        print('\t' + f"Precision: {p}") 
+        print('\t' + f"Recall: {r}") 
+        
+    
+    
+
+def save_model(model, model_filepath):
+    
+    pickle.dump(model, open(model_filepath, 'wb'))
+    
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
